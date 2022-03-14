e93907106510a3c147169739a9a016cf67baded7
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-03-14 13:43:13 -0300

    Navbar menu toggle tbn

--- js/script.js
+++ js/script.js
@@ -1,6 +1,20 @@
-/* Search form/Formulário de pesquisa*/
+/* Search form/Formulário de pesquisa btn*/
 let searchForm = document.querySelector('.search-form');
 
 document.querySelector('#search-btn').onclick = () => {
     searchForm.classList.toggle('active');
+    navbar.classList.remove('active');
+}
+
+/* Menu toggle/menu toggle tbn*/
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick = () => {
+    navbar.classList.toggle('active');
+    searchForm.classList.remove('active');
+}
+
+windows.onscroll = () => {
+    searchForm.classList.remove('active');
+    navbar.classList.remove('active');
 }
