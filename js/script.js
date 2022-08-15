ae9ac6c4c419a9f0ffdaa4fad364a88ab85dee1e
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-08-15 15:48:48 -0300

    Update

--- js/script.js
+++ js/script.js
@@ -18,3 +18,19 @@ windows.onscroll = () => {
     searchForm.classList.remove('active');
     navbar.classList.remove('active');
 }
+
+/* Swiper-js Slider */
+var swiper = new Swiper(".home-slider", {
+    centeredSlides: true,
+    loop:true,
+    autoplay: {
+      delay: 9500,
+      disableOnInteraction: false,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+  
+  
