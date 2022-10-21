9e375bc6953a1fa37dbe817b483aaf499725b33a
Daniel Oliveira <danielsapup3@gmail.com>
2022-10-21 17:03:19 -0300

    Update script

--- js/script.js
+++ js/script.js
@@ -19,6 +19,32 @@ window.onscroll = () => {
     navbar.classList.remove('active');
 }
 
+/* Filter-Buttons */
+let filterBtn = document.querySelectorAll('.filter-buttons .buttons');
+let filterItem = document.querySelectorAll('.products .box-container .box');
+
+filterBtn.forEach(button =>{
+
+  button.onclick = () => {
+    filterBtn.forEach(btn => btn.classList.remove('active'));
+    button.classList.add('active');
+
+    let dataFilter = button.getAttribute('data-filter');
+    
+    filterItem.forEach(item => {
+
+      item.classList.remove('active');
+      item.classList.add('hide');
+
+      if(item.getAttribute('data-item') == dataFilter || dataFilter == 'all'){
+        item.classList.remove('active');
+        item.classList.add('hide');
+      }
+    });
+  }
+
+});
+
 /* Swiper-js Slider */
 var swiper = new Swiper(".home-slider", {
     centeredSlides: true,
